c5bf4435c5431d1df3f6fbb69325ddc841062efa
马丽捷 <37659570+marry11@users.noreply.github.com>
2018-07-20 13:31:37 +0800

    1
    alert1

--- LinkLend/LinkLendFirst.js
+++ LinkLend/LinkLendFirst.js
@@ -1,34 +1,18 @@
-function backcon(){
-    alert("1");
-    $.ajax({
-        type:'post',
-        url:'../ceshi.php', //测试接口 陈婷给的测试接口
-
-        success:function(result){  
-            result = JSON.parse(result);   
-                console.log(result); 
-                alert(1);           
-                if (result.regStatus == true ){
-                    if(result.hireStatus==true){
-                        alert("表示有正在租用的网，跳转到“未连接”的页面");
-                        window.location.href="../LinkRent/LinkRent.html?rent=1"; //rent参数为跳转下一个页面后做判断
-                    }else{
-                        alert("跳转到“暂未借用账号”页面");
-                        window.location.href="../LinkRent/LinkRent.html?rent=0"; 
-                    }
-                }else{          
-                    //登录状态失效
-                    window.location.href="../LinkLogin/index.html";
-                }
-            },
-        error:function(XMLHttpRequest, textStatus, errorThrown){
-                alert(errorThrown);        
-            },
-        }); 
-}
-
 function clickfunt(){
-var date1=document.getElementById("startTime").value;
+    $(document).ready(function(){
+    $("button").click(function(){
+        // $("#alert").fadeToggle();
+        $(function () {  
+    setTimeout(function () {  
+         $("#alert").fadeIn(3000);
+        // $("#alert").show();  
+    }, 1000); });
+        // setTimeout("$(this).hide()",1000);
+        // $("#alert").fadeToggle("slow");
+        // $("#alert").fadeToggle(3000);
+    });
+    });
+    var date1=document.getElementById("startTime").value;
     var date2=document.getElementById("overTime").value;
     var name=$("#name").val();
     var password=$("#password").val();
@@ -39,12 +23,61 @@ var date1=document.getElementById("startTime").value;
     var allTime=$("#allTime").val();
     var price=$("#price").val();
     var accept = document.getElementById('accept').checked;
-    if(name.length == 0) alert("用户名不能为空");
-    else if(!idreg.test(name)) alert("用户名格式不正确");
-    else if(password.length == 0) alert("密码不能为空");
-    else if(price.length == 0) alert("价钱不能为空");
-    else if(!preg.test(price)) alert("价钱格式不正确或价钱不能超过10元");
-    else if(accept == false) alert("您需要仔细阅读并同意接收用户使用协议！");
+    if(name.length == 0) {
+        $('#alert').empty();
+         var html = '';
+         html += '<div id="p1" style="height:2rem;line-height:2rem;background-color:#E51C23;font-size:1rem;">未填写账号</div>'
+        $('#alert').html(html);
+         console.log(html);
+    // document.getElementById('name').innerHTML='<img src="../image/alert.png" />';
+    // alert("用户名不能为空");
+}
+    else if(!idreg.test(name)) 
+    // document.getElementById("nameinfo").innerHTML = "姓名不能为空";  
+{
+    $('#alert').empty();
+         var html = '';
+         html += '<div id="p1" style="height:2rem;line-height:2rem;background-color:#E51C23;font-size:1rem;">账号格式不正确</div>'
+        $('#alert').html(html);
+         console.log(html);
+}
+        // alert("用户名格式不正确");
+    else if(password.length == 0) 
+{
+    $('#alert').empty();
+         var html = '';
+         html += '<div id="p1" style="height:2rem;line-height:2rem;background-color:#E51C23;font-size:1rem;">未填写密码</div>'
+        $('#alert').html(html);
+         console.log(html);
+}
+        // alert("密码不能为空");
+    else if(price.length == 0)
+    {
+        $('#alert').empty();
+         var html = '';
+         html += '<div id="p1" style="height:2rem;line-height:2rem;background-color:#E51C23;font-size:1rem;">未填写价钱</div>'
+        $('#alert').html(html);
+         console.log(html);
+    } 
+    // alert("价钱不能为空");
+    else if(!preg.test(price))
+{
+    $('#alert').empty();
+         var html = '';
+         html += '<div id="p1" style="height:2rem;line-height:2rem;background-color:#E51C23;font-size:1rem;">价钱格式不正确或价钱不能超过10元</div>'
+        $('#alert').html(html);
+         console.log(html);
+}
+        // alert("价钱格式不正确或价钱不能超过10元");
+    else if(accept == false) 
+{
+    $('#alert').empty();
+         var html = '';
+         html += '<div id="p1" style="height:2rem;line-height:2rem;background-color:#E51C23;font-size:1rem;">您需要仔细阅读并同意接收用户使用协议！</div>'
+        $('#alert').html(html);
+         console.log(html);
+}
+        // alert("您需要仔细阅读并同意接收用户使用协议！");
     else{
         $.ajax({
             type:'post',
@@ -63,7 +96,8 @@ var date1=document.getElementById("startTime").value;
                     if (result.regStatus){//登陆状
                         alert("登录状态在线");
                         if(result.chuzuStatus){
-                            alert("出租成功！");
+                           
+                            // alert("出租成功！");
                             window.location.href="../LinkLendStatus/LinkLendNone.html?startTime="+startTime+"&overTime="+overTime+"&allTime="+allTime;
                         }else{
                             alert("出租失败！");
